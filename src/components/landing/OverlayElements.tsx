547c81d495876bcf8804b0b2cc5ea142f3bd3095
abdishakuuralimohamed <abdishakuuralimohamed@gmail.com>
2026-04-02 22:57:04 +0100

    fix: remove social proof toast notifications
    - Remove social proof popups from landing page
    - Keeps WhatsApp floating button only

--- src/components/landing/OverlayElements.tsx
+++ src/components/landing/OverlayElements.tsx
@@ -3,14 +3,10 @@
 import dynamic from "next/dynamic";
 import { WhatsAppFloat } from "./WhatsAppFloat";
 
-const SocialProof = dynamic(() => import("./SocialProof").then(mod => mod.SocialProof), {
-    ssr: false,
-});
-
 export function OverlayElements() {
     return (
         <>
-            <SocialProof />
+            <WhatsAppFloat />
         </>
     );
 }
