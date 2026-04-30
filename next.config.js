ddd33cbad9241d7a1c629acd9f3a40422ef6c346
abdishakuuralimohamed <abdishakuuralimohamed@gmail.com>
2026-05-01 00:30:23 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -78,7 +78,7 @@ const nextConfig = {
           { key: "Cross-Origin-Opener-Policy", value: "same-origin-allow-popups" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "origin-when-cross-origin" },
-           { key: "Permissions-Policy", value: "camera=(self), microphone=(self), geolocation=(self)" },
+           { key: "Permissions-Policy", value: "camera=*, microphone=*, geolocation=*" },
         ],
       },
       {
